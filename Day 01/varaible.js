de9f654760b53d9f3f6c436cbced1f8d889c17da
Nsima George <georgensima@gmail.com>
2020-07-31 20:58:34 +0800

    Day One
    Understanding the console and Javascript Variables.

---
+++ Day 01/varaible.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'George'    // first name of a person
+let lastName = 'Nsima'      // last name of a person
+let country = 'Nigeria'       // country
+let city = 'Abuja'         // capital city
+let age = 'Old'                 // age in years
+let isMarried = false
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Nsima',     //name of a person
+  job = 'Programmer',
+  live = 'Nigeria'
