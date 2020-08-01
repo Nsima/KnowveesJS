65dcd6a8e68b4daa4440cd181dc63b3aebddae89
Nsima George <georgensima@gmail.com>
2020-08-02 07:19:44 +0800

    Day 03
    Booleans and Operators

---
+++ Day03/Day03.md
@@ -0,0 +1,7 @@
+<div align="center">
+  <h1> 30 Days Of JavaScript: Booleans, Operators, Date</h1>
+</div>
+
+[<< Day 2](../Day02/day02.md) | [Day 4 >>](../Day04/day04.md)
+
+#### I had to do little on this day. I decided it was just a repetition of most languages. *Don't worry Assembly language writers, you are not included in this listing*
---
+++ Day03/starter/scripts/main.js
@@ -0,0 +1,115 @@
+// this is your main.js script
+let isLightOn = true
+let isRaining = false
+let isHungry = false
+let isMarried = true
+let truValue = 4 > 3    // true
+let falseValue = 4 < 3  // false
+
+//Undefined Variable
+let firstName
+console.log(firstName) //not defined, because it is not assigned to a value yet
+
+//Null Variable
+let empty = null
+console.log(empty) // -> null , means no value
+
+//Assignment Operators
+let firstName = 'George'
+let country = 'Nigeria'
+
+//Arithmetic Operators
+let numOne = 4
+let numTwo = 3
+let sum = numOne + numTwo
+let diff = numOne - numTwo
+let mult = numOne * numTwo
+let div = numOne / numTwo
+let remainder = numOne % numTwo
+let powerOf = numOne ** numTwo
+
+console.log(sum, diff, mult, div, remainder, powerOf) // 7,1,12,1.33,1, 64
+
+const PI = 3.14
+let radius = 100          // length in meter
+
+//Let us calculate area of a circle
+const areaOfCircle = PI * radius * radius
+console.log(areaOfCircle)  //  314 m
+
+
+const gravity = 9.81      // in m/s2
+let mass = 72             // in Kilogram
+
+// Let us calculate weight of an object
+const weight = mass * gravity
+console.log(weight)        // 706.32 N(Newton)
+
+const boilingPoint = 100  // temperature in oC, boiling point of water
+const bodyTemp = 37       // body temperature in oC
+
+
+// Concatenating string with numbers using string interpolation
+/*
+ The boiling point of water is 100 oC.
+ Human body temperature is 37 oC.
+ The gravity of earth is 9.81 m/s2.
+ */
+console.log(
+  `The boiling point of water is ${boilingPoint} oC.\nHuman body temperature is ${bodyTemp} oC.\nThe gravity of earth is ${gravity} m / s2.`
+)
+
+//Comparison Operators
+console.log(3 > 2)              // true, because 3 is greater than 2
+console.log(3 >= 2)             // true, because 3 is greater than 2
+console.log(3 < 2)              // false,  because 3 is greater than 2
+console.log(2 < 3)              // true, because 2 is less than 3
+console.log(2 <= 3)             // true, because 2 is less than 3
+console.log(3 == 2)             // false, because 3 is not equal to 2
+console.log(3 != 2)             // true, because 3 is not equal to 2
+console.log(3 == '3')           // true, compare only value
+console.log(3 === '3')          // false, compare both value and data type
+console.log(3 !== '3')          // true, compare both value and data type
+console.log(3 != 3)             // false, compare only value
+console.log(3 !== 3)            // false, compare both value and data type
+console.log(0 == false)         // true, equivalent
+console.log(0 === false)        // false, not exactly the same
+console.log(0 == '')            // true, equivalent
+console.log(0 == ' ')           // true, equivalent
+console.log(0 === '')           // false, not exactly the same
+console.log(1 == true)          // true, equivalent
+console.log(1 === true)         // false, not exactly the same
+console.log(undefined == null)  // true
+console.log(undefined === null) // false
+console.log(NaN == NaN)         // false, not equal
+console.log(NaN === NaN)        // false
+console.log(typeof NaN)         // number
+
+console.log('mango'.length == 'avocado'.length)  // false
+console.log('mango'.length != 'avocado'.length)  // true
+console.log('mango'.length < 'avocado'.length)   // true
+console.log('milk'.length == 'meat'.length)      // true
+console.log('milk'.length != 'meat'.length)      // false
+console.log('tomato'.length == 'potato'.length)  // true
+console.log('python'.length > 'dragon'.length)   // false
+
+//Logical Operators
+// && ampersand operator example
+
+const check = 4 > 3 && 10 > 5         // true && true -> true
+const check = 4 > 3 && 10 < 5         // true && false -> false
+const check = 4 < 3 && 10 < 5         // false && false -> false
+
+// || pipe or operator, example
+
+const check = 4 > 3 || 10 > 5         // true  || true -> true
+const check = 4 > 3 || 10 < 5         // true  || false -> true
+const check = 4 < 3 || 10 < 5         // false || false -> false
+
+//! Negation examples
+
+let check = 4 > 3                     // true
+let check = !(4 > 3)                  //  false
+let isLightOn = true
+let isLightOff = !isLightOn           // false
+let isMarried = !false                // true
